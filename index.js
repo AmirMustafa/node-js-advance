1ce61eb2ee56925cd33861a838114ec39e0ca475
Amir Mustafa <amirengg15@gmail.com>
2022-09-11 01:41:37 +0530

    feat: replace callback hell with promises

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-function delay(seconds, callback) {
-    setTimeout(callback, seconds * 1000);
-};
+var delay = (seconds) => new Promise((resolve, reject) => {
+    setTimeout(resolve, seconds * 1000);
+});
 
 console.log('starting delays');
 
-delay(2, () => {
+delay(2).then(() => {
     console.log('two seconds');
-
-    delay(1, () => {
-        console.log('three seconds');
-
-        delay(1, () => {
-            console.log('four seconds');
-        })
-    })
 });
 
 
-// function delay(seconds, callback) {
+// Callback Hell
+// var delay = (seconds, callback) => {
 //     setTimeout(callback, seconds * 1000);
 // };
 
@@ -26,41 +19,3 @@ delay(2, () => {
 // delay(2, () => {
 //     console.log('two seconds');
 // });
-
-
-
-// function hideString(str, done) {
-//     process.nextTick(() => {
-//         done(str.replace(/[a-zA-Z]/g, 'X'));
-//     });
-// }
-
-// hideString("Hello World",  (hidden) => {
-//     console.log(hidden);   // RUNS SECOND
-// });
-
-// console.log('end');   // RUNS FIRST
-
-
-// function hideString(str, done) {
-//     done(str.replace(/[a-zA-Z]/g, 'X'));
-// }
-
-// hideString("Hello World",  (hidden) => {
-//     console.log(hidden);  // RUNS FIRST
-// });
-
-// console.log('end');      // RUNS SECOND
-
-
-
-
-
-// EG 1: Synchronous
-// function hideString(str) {
-//     return str.replace(/[a-zA-Z]/g, 'X');
-// }
-
-// var hidden = hideString("Hello World");   // RUNS FIRST
-
-// console.log( hidden );      // RUNS SECOND
