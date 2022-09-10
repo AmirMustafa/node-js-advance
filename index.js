95e4905e275be7ebceba22d038f224440a9ac7a8
Amir Mustafa <amirengg15@gmail.com>
2022-09-10 21:05:12 +0530

    feat: callback pattern - process.nextTick() - async

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-
-
-
 function hideString(str, done) {
-    done(str.replace(/[a-zA-Z]/g, 'X'));
+    process.nextTick(() => {
+        done(str.replace(/[a-zA-Z]/g, 'X'));
+    });
 }
 
 hideString("Hello World",  (hidden) => {
-    console.log(hidden);
+    console.log(hidden);   // RUNS SECOND
 });
 
-console.log('end');
+console.log('end');   // RUNS FIRST
+
+
+// function hideString(str, done) {
+//     done(str.replace(/[a-zA-Z]/g, 'X'));
+// }
+
+// hideString("Hello World",  (hidden) => {
+//     console.log(hidden);  // RUNS FIRST
+// });
+
+// console.log('end');      // RUNS SECOND
+
+
+
+
 
 // EG 1: Synchronous
 // function hideString(str) {
 //     return str.replace(/[a-zA-Z]/g, 'X');
 // }
 
-// var hidden = hideString("Hello World");
+// var hidden = hideString("Hello World");   // RUNS FIRST
 
-// console.log( hidden );
+// console.log( hidden );      // RUNS SECOND
