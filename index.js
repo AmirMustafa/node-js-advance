5e2ca23f310b40037e72c6c8974e1529641a62c9
Amir Mustafa <amirengg15@gmail.com>
2022-09-10 23:25:18 +0530

    feat: adding delays in syncronous callback calls

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ console.log('starting delays');
 
 delay(2, () => {
     console.log('two seconds');
+
+    delay(1, () => {
+        console.log('three seconds');
+
+        delay(1, () => {
+            console.log('four seconds');
+        })
+    })
 });
+
+
+// function delay(seconds, callback) {
+//     setTimeout(callback, seconds * 1000);
+// };
+
+// console.log('starting delays');
+
+// delay(2, () => {
+//     console.log('two seconds');
+// });
 
 
 
