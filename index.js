916e3325436c1fe25e1400657160cec52dbb0387
Amir Mustafa <amirengg15@gmail.com>
2022-09-24 10:58:12 +0530

    feat: promisify - convert callback into promise

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
+var { promisify } = require('util');
+
+var delay = (seconds, callback) => {
+  if(seconds > 3) {
+    callback(new Error(`${seconds} is too long`));
+  } else {
+    setTimeout(callback(null, `the ${seconds} delay is over.`, seconds*1000));
+  }
+};
+
+var promiseDelay = promisify(delay);
+
+promiseDelay(6)
+  .then(console.log)
+  .catch((err) => console.log(`error: ${err.message}`))
+
+// delay(4, (error, message) => {
+//   if(error) {
+//     console.log(error.message);
+//   } else {
+//     console.log(message);
+//   }
+// });
+
+
+
+// ----------------------------------------------------------------
+
+
 // DYNAMIC DATA PASS
-var delay = (seconds) => new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('message after delay finishes');
-    }, seconds * 1000);
-});
+// var delay = (seconds) => new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('message after delay finishes');
+//     }, seconds * 1000);
+// });
 
-console.log('starting delays');
+// console.log('starting delays');
 
-// We can chain as many promise here but in the neat way
-delay(2)
-  .then(console.log )
-  .then(() => 42)
-  .then((number) => console.log(`hello world ${number}`));
+// // We can chain as many promise here but in the neat way
+// delay(2)
+//   .then(console.log )
+//   .then(() => 42)
+//   .then((number) => console.log(`hello world ${number}`));
 // delay(2).then((message) => console.log(message));
 
 
