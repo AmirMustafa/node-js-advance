996bd1c7b577058f406fafa2782c7893fbf951f3
Amir Mustafa <amirengg15@gmail.com>
2022-10-03 20:30:20 +0530

    feat: readable streams - buffer mode, string mode

---
+++ index.js
@@ -0,0 +1,36 @@
+import { Readable } from 'stream';
+
+const peaks = [
+    "Tallac",
+    "Ralston",
+    "Rubicon",
+    "Twin Peaks",
+    "Castle Peaks",
+    "Rose",
+    "Freel Peak"
+];
+
+class StreamFromArray extends Readable {
+    constructor (array) {
+        super({ encoding: 'UTF-8' });  // if we will pass UTF-8 - string mode, if kept empty - buffer mode
+        this.array = array;
+        this.index = 0;
+    }
+
+    _read() {
+        if(this.index <= this.array.length) {
+            const chunk = this.array[this.index];
+            this.push(chunk);
+            this.index += 1;
+        } else {
+            this.push(null);
+        }
+    }
+}
+
+const peakStream = new StreamFromArray(peaks);
+
+peakStream.on('data' , (chunk) => console.log(chunk));
+
+peakStream.on('end', () => console.log('done !'));
+
