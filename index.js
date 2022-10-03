67d11ad59e0eba61abae418b96f23a7e54e5171f
Amir Mustafa <amirengg15@gmail.com>
2022-10-03 21:13:14 +0530

    feat: reading filesystem streams - createReadStream (i.e. reading video)

--- index.js
+++ index.js
@@ -1,41 +1,67 @@
-import { Readable } from 'stream';
+import fs from 'fs';
 
-const peaks = [
-    "Tallac",
-    "Ralston",
-    "Rubicon",
-    "Twin Peaks",
-    "Castle Peaks",
-    "Rose",
-    "Freel Peak"
-];
+const readStream = fs.createReadStream("./powder-day.mp4");
 
-class StreamFromArray extends Readable {
-    constructor (array) {
-        super({ objectMode: true });
-        // super({ encoding: 'UTF-8' });  // if we will pass UTF-8 - string mode, if kept empty - buffer mode
-        this.array = array;
-        this.index = 0;
-    }
+readStream.on("data", (chunk) => {
+    console.log('size: ' + chunk.length);
+    // console.log("read data ==>", chunk);
+});
 
-    _read() {
-        if(this.index <= this.array.length) {
-            const chunk = {
-                data: this.array[this.index],
-                index: this.index
-            };
-            // const chunk = this.array[this.index];
-            this.push(chunk);
-            this.index += 1;
-        } else {
-            this.push(null);
-        }
-    }
-}
+readStream.on("end", () => {
+    console.log("read stream finished.");
+});
 
-const peakStream = new StreamFromArray(peaks);
+readStream.on("error", (error) => {
+    console.log("an error occured.");
+    console.error(error)
+});
 
-peakStream.on('data' , (chunk) => console.log(chunk));
 
-peakStream.on('end', () => console.log('done !'));
+
+
+
+
+
+
+
+// import { Readable } from 'stream';
+
+// const peaks = [
+//     "Tallac",
+//     "Ralston",
+//     "Rubicon",
+//     "Twin Peaks",
+//     "Castle Peaks",
+//     "Rose",
+//     "Freel Peak"
+// ];
+
+// class StreamFromArray extends Readable {
+//     constructor (array) {
+//         super({ objectMode: true });
+//         // super({ encoding: 'UTF-8' });  // if we will pass UTF-8 - string mode, if kept empty - buffer mode
+//         this.array = array;
+//         this.index = 0;
+//     }
+
+//     _read() {
+//         if(this.index <= this.array.length) {
+//             const chunk = {
+//                 data: this.array[this.index],
+//                 index: this.index
+//             };
+//             // const chunk = this.array[this.index];
+//             this.push(chunk);
+//             this.index += 1;
+//         } else {
+//             this.push(null);
+//         }
+//     }
+// }
+
+// const peakStream = new StreamFromArray(peaks);
+
+// peakStream.on('data' , (chunk) => console.log(chunk));
+
+// peakStream.on('end', () => console.log('done !'));
 
