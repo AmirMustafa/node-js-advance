63e63e8678a49c2beae514f89af3a8b2e4d004ff
Amir Mustafa <amirengg15@gmail.com>
2022-10-09 18:29:30 +0530

    feat: sending video information to server

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import { createServer } from 'http';
-import { createReadStream } from 'fs';
+import { stat, createReadStream } from 'fs';
+import { promisify } from 'util';
 const fileName = 'powder-day.mp4';
+const fileInfo = promisify(stat);
 
-createServer((req, res) => {
-    res.writeHead(200, {  'Content-Type': 'video/mp4'});
+createServer(async (req, res) => {
+    const { size } = await fileInfo(fileName);
+    res.writeHead(200, {
+       'Content-Length': size,
+       'Content-Type': 'video/mp4'
+    });
     createReadStream(fileName).pipe(res);
 
 }).listen(3000, () => console.log('Server listening on port - 3000'));
